e127feb5c1e778e56f76c3f98a10118c701f000d
Dalvir <ilya.hurs@gmail.com>
2023-01-26 22:03:03 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array = new string[]{"hello","2","world",":-)","2134","-2"};
+string[] array2 = new string[0];
+for (int i = 0; i < array.Length; i++){
+    if (array[i].Length < 4){
+        array2 = ArrayAppenderLengher(array2,array[i]);
+    }
+}
+PrintArray(array2);
+
+// заменить существующий массив новым больше на 1 значение и заполнить.
+// в теории старый должен стиратся из памяти.
+string[] ArrayAppenderLengher(string[] ar,string str){
+    string[] array = new string[ar.Length+1];
+    ar.CopyTo(array,0);
+    array[array.Length-1] = str;
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }   
+    Console.WriteLine();
+}
